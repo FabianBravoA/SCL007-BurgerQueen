7270d4f5a9ebd37879a421187a3fe2a8dd2d5d2b
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-03-10 00:19:23 -0300

    Instalaciòn Angular, Creacion de componentes, creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { ChefComponent } from './components/chef/chef.component';
+import { ProductsComponent } from './components/products/products.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { Page404Component } from './components/page404/page404.component';
+import { FormsModule } from '@angular/forms'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'waiter', component: WaiterComponent},
+  {path: 'chef' , component: ChefComponent},
+  {path: 'products', component: ProductsComponent },
+  {path: 'user/login', component: LoginComponent},
+  {path: 'user/profile', component: ProfileComponent},
+  {path: '**', component: Page404Component}  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule
+  ],
+  exports: [RouterModule],
+  })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { Page404Component } from './components/page404/page404.component';
+import { ProductsComponent } from './components/products/products.component';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { ChefComponent } from './components/chef/chef.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    Page404Component,
+    ProductsComponent,
+    WaiterComponent,
+    ChefComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
